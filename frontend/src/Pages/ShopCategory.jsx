33d345c3a544a5af021398016c9f1f2be545899a
Yuvraj Singh <yuvrajsteve@email.com>
2026-07-05 13:17:11 +0530

    fixed category case matching in shop category

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react'
-import './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 import Item from '../Components/Item/Item'
